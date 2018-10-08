fb2d691e180b98b920932dbefacbad60854e9a7e
khuf <knut-95@hotmail.com>
2018-10-08 14:16:01 +0200

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit c4601bf965c86c9934869a815888d199bec76c00, reversing
    changes made to cf0954e53f9b7527e506a5e6a8c07d706737f8fd.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import NavBar from "./components/navbar";
-import "./index.css";
-import "./components/navbar.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<NavBar />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
