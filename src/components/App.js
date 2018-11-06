ac4235b24fa81b8f1d77e95905aa80959b423cb6
khuf <knut-95@hotmail.com>
2018-11-06 11:13:59 +0100

    Removed account page

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,6 @@ import "bootstrap/dist/css/bootstrap.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "./App.css";
 import WelcomePage from "./welcome_page/WelcomePage";
-import AccountPage from "./AccountPage";
 import SignUpPage from "./SignUpPage";
 import * as routes from "../constants/routes";
 import withAuthentication from "./withAuthentication";
@@ -19,8 +18,6 @@ const App = () => (
       <Route exact path={routes.SIGN_UP} component={SignUpPage} />
       <Route exact path={routes.SIGN_IN} component={WelcomePage} />
       <Route exact path={routes.HOME} component={Dashboard} />
-      <Route exact path={routes.ACCOUNT} component={AccountPage} />
-      <Route exact path={routes.PROGRESS} component={AccountPage} />
       <Route exact path={routes.CARD_MODAL} component={cardModal} />
       <Route exact path={routes.CARD_TEMPLATE} component={cardTemplate} />
     </div>
